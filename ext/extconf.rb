5f78ad91bde67653f864d443dd5a34bd81e5fffc
Zete Lui <mas@k.ed>
2013-05-13 01:05:21 +0800

    add fmemopen as submodule for scanf extension

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,3 +1,10 @@
 require "mkmf"
+require "fileutils"
+
+if RUBY_PLATFORM !~ /linux/
+  Dir.chdir File.dirname(__FILE__) do
+    FileUtils.cp 'fmemopen/fmemopen.*', '.'
+  end
+end
 
 create_makefile 'zscan'
